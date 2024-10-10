5880a6ae8476c324394b38c3aba570b730072706
AESolodchuk <Solodchuk.Aleksey@gmail.com>
2024-10-10 15:58:56 +0300

    update

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -3,8 +3,6 @@ import css from './MovieList.module.css'
 
 const MovieList = ({ movie, location }) => 
     
-        <Link to={`/movies/${movie.id}`} className={css.link} state={location}>{movie.title}</Link>
-
-               
+        <Link to={`/movies/${movie.id}`} className={css.link} state={location}>{movie.title}</Link>               
        
 export default MovieList
